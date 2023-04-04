61cd1ba473fa1c555750d7dc6d0a19eb385c08c8
Michael Taggart <mtaggart@taggart-tech.com>
2023-04-04 06:29:20 -0700

    Rebuild: Tue Apr  4 06:29:19 AM PDT 2023

--- docs/docs/docs/docs/docs/docs/build.sh
+++ docs/docs/docs/docs/docs/docs/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Make docs folder if it doesn't exist
+if [ ! -d docs ]; then
+    mkdir docs
+fi
+
 # Rebuild the site
 npx honkit build
 
 # Copy the `_book` folder contents to `docs`
-cp -Rf _book docs
+cp -Rf _book/* docs/
 
 # Create a new commit for the rebuild (every time), maybe with a date?
 git add docs/
