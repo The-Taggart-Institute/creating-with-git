aeacfe39b9ecf13307a4be28d8db5aac1ad259ad
Michael Taggart <mtaggart@taggart-tech.com>
2023-04-04 20:31:17 -0700

    Rebuild: Tue Apr  4 08:31:17 PM PDT 2023

--- docs/build.sh
+++ docs/build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Make docs folder if it doesn't exist
-if [ ! -d docs ]; then
-    mkdir docs
+if [ -d docs ]; then
+    rm -rf docs
 fi
 
 # Rebuild the site
 npx honkit build
 
 # Copy the `_book` folder contents to `docs`
-cp -Rf _book/* docs/
+cp -Rf _book docs
+
+# Readd CNAME file
+echo cwg.taggartinstitute.org > docs/CNAME
 
 # Create a new commit for the rebuild (every time), maybe with a date?
 git add docs/
